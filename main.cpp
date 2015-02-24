935ee5d09756f8b76a313448feae510bdfe288b1
Kamil Maciejczek <maciejczek.kamil@gmail.com>
2015-02-24 23:04:53 +0100

    Added spirit parsing

--- main.cpp
+++ main.cpp
@@ -1,10 +1,34 @@
+#include <fstream>
+#include <sstream>
 #include <iostream>
 #include "JsonGrammar.h"
 
 using namespace std;
+using namespace boost::spirit;
 
-int main() {
-    cout << "Hello, World!" << endl;
+string ReadFile(const char* fileName)
+{
+    std::ifstream fs(fileName);
+    std::ostringstream ss;
+    ss << fs.rdbuf();
+    std::string data = ss.str();
+}
+
+int main(int argc, char *argv[])
+{
+    std::string data = ReadFile(argv[1]);
+
+    JsonGrammar grammar;
+    auto info = parse(data.c_str(), grammar, boost::spirit::space_p);
+    if (info.hit)
+    {
+        if (info.full)
+            std::cout << "parsed" << std::endl;
+        else
+            std::cout << "parsed partially" << std::endl;
+    }
+    else
+        std::cout << "parsing error: '" << info.stop << "'" << std::endl;
 
     return 0;
 }
