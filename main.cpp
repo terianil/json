761d02ded3dadf1d70853b0608b152f4ad8beec5
Kamil Maciejczek <maciejczek.kamil@gmail.com>
2015-02-25 07:41:04 +0100

    Added load tests

--- main.cpp
+++ main.cpp
@@ -6,9 +6,32 @@
 
 using namespace std;
 
+void LoadTest(const char* file)
+{
+    auto mem1 = GetMemorySize();
+    auto time1 = Time();
+
+    auto json = new Json(file);
+
+    auto time2 = Time();
+
+    auto mem2 = GetMemorySize();
+    cout << "mem, kB: " << GetMemorySize() - mem1 << endl;
+    cout << "loading time, s: " << TimeDiff(time1, time2).tv_sec << " ,ms: " << TimeDiff(time1, time2).tv_nsec / 1024 / 1024 << endl;
+}
+
+void LoadTests()
+{
+    LoadTest("/home/terianil/Desktop/json/test/100k.json");
+    LoadTest("/home/terianil/Desktop/json/test/1000k.json");
+    LoadTest("/home/terianil/Desktop/json/test/10000k.json");
+}
+
 
 int main(int argc, const char* argv[])
 {
+    //LoadTests();
+
     auto mem1 = GetMemorySize();
     auto time1 = Time();
 
@@ -17,14 +40,14 @@ int main(int argc, const char* argv[])
     auto time2 = Time();
 
     auto mem2 = GetMemorySize();
-    cout << "mem: " << GetMemorySize() - mem1 << endl;
-    cout << "loading time, ms: " << TimeDiff(time1, time2).tv_nsec / 1024 / 1024 << endl;
+    cout << "mem, kB: " << GetMemorySize() - mem1 << endl;
+    cout << "loading time, s: " << TimeDiff(time1, time2).tv_sec << " ,ms: " << TimeDiff(time1, time2).tv_nsec / 1024 / 1024 << endl;
 
     cout << "----------Json members:--------" << endl;
     json->PrintKeys();
     cout << endl;
 
-    /*cout << "imie: " << json->GetMemberValue<string>("imie") << endl;
+    cout << "imie: " << json->GetMemberValue<string>("imie") << endl;
 
     cout << "'numer' type is double: " << json->CheckValueType<double>("numer") << endl;
     cout << "'numer' type is string: " << json->CheckValueType<string>("numer") << endl;
@@ -43,7 +66,7 @@ int main(int argc, const char* argv[])
     for (auto e:json->GetArrayOfType<double>("array"))
     {
         cout << "array element: " << e << endl;
-    }*/
+    }
 
     return 0;
 }
